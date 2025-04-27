c221e661ffb8e67e23b4637acf0bdbd9905a2266
MekJohn <gaudio90@hotmail.it>
2025-04-27 09:55:52 +0200

    init

---
+++ xwall.py
@@ -0,0 +1,156 @@
+# AutoPyrewall 2025
+
+import subprocess
+import sys
+import ctypes
+import pathlib as pt
+import time as tm
+
+
+def is_admin():
+    """
+    Verifica se lo script è in esecuzione con privilegi di amministratore.
+    Restituisce True se l'utente è un amministratore, False altrimenti.
+    """
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def run_as_admin(executable: str = None, argv: list = None):
+    """
+    Riavvia lo script con privilegi di amministratore.
+    """
+    # TODO i percorsi devono essere assoluti altrimenti non trova nulla
+    python_executable = sys.executable
+    script_path = pt.Path(sys.argv[0]).resolve()
+    script_arguments = " ".join(sys.argv[1:]) if len(sys.argv) > 1 else ""
+                 
+    if not is_admin():
+        print("Richiesta di privilegi di amministratore...")
+        print(f"{python_executable}\n{script_path}\n{script_arguments}")
+        arguments_to_pass = f'"{script_path}" {script_arguments}'
+        returned = ctypes.windll.shell32.ShellExecuteW(None, "runas", python_executable, arguments_to_pass, None, 1)
+        
+        print(returned)
+        success = True if int(returned) > 32 else False
+        if success:
+            print("Ran as admin correctly.")
+        else:
+            print("Failed to run as admin.")
+        sys.exit()
+
+            
+            
+            
+def block_exe_traffic(folder_path):
+    """
+    Blocca tutto il traffico di rete in entrata e in uscita per i file .exe
+    all'interno della cartella specificata.  Crea regole del firewall di Windows.
+
+    Args:
+        folder_path (str): Il percorso della cartella contenente i file .exe da bloccare.
+    """
+    
+    folder_path = pt.Path(folder_path)
+    try:
+        # Verifica che il percorso della cartella esista
+        if not folder_path.is_dir():
+            print(f"Errore: La cartella specificata '{folder_path}' non esiste.")
+            sys.exit(1)
+            
+        exe_filelist = [file for file in folder_path.iterdir() if file.suffix == ".exe"]
+        # Scorre tutti i file nella cartella
+        for file in exe_filelist:
+            
+            time_now = int(tm.time())
+            time_iso = tm.ctime(time_now)
+            
+            ADD_RULE_CMD = ["netsh", "advfirewall", "firewall", "add", "rule"]
+            SETTINGS = ["enable=yes", "profile=any"]
+            # Blocca il traffico in entrata per il file .exe
+            subprocess.run(
+                [*ADD_RULE_CMD, "dir=in", "action=block",
+                 f'name=APW-{time_now}-BKI-{file.stem}',  #suffisso _In per la regola in entrata
+                 f'program={file.absolute()}',
+                 f"description=Autofirewall generated rule at {time_iso} by Python process.",
+                 *SETTINGS] ,
+                check=True
+            )
+
+            # Blocca il traffico in uscita per il file .exe
+            subprocess.run(
+                [*ADD_RULE_CMD, "dir=out", "action=block",
+                 f'name=APW-{time_now}-BKO-{file.stem}', #suffisso _Out per la regola in uscita
+                 f"program={file.absolute()}",
+                 f"description=Autofirewall generated rule at {time_iso} by Python process.",
+                 *SETTINGS] ,
+                check=True
+            )
+            print(f"Traffico bloccato per: {file}")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante la creazione delle regole del firewall: {e}")
+        print("Assicurarsi di avere i privilegi di amministratore e che i comandi siano stati inseriti correttamente.")
+        sys.exit(1)
+    except Exception as ex:
+        print(f"Si è verificato un errore imprevisto: {ex.with_traceback()}")
+        sys.exit(1)
+
+
+
+def list_firewall_rules(options: list = []):
+    """
+    Elenca tutte le regole del firewall di Windows e le restituisce come una lista di dizionari.
+    Ogni dizionario rappresenta una regola del firewall.
+    """
+    try:
+        # Esegue il comando netsh per ottenere tutte le regole del firewall
+        NETSH_CMD = ["netsh", "advfirewall", "firewall", "show rule", "name=all", *options, "verbose"]
+        result = subprocess.run(NETSH_CMD, check=True, capture_output=True, text = True)        
+        
+        output = result.stdout
+        output_lines = [line.strip() for line in output.strip().split('\n') if line.strip() != ""]
+
+        # Analizza l'output di netsh
+        rules = []
+        current_rule = {}
+        for line in output_lines:
+            if ":" in line:
+                key, value = line.split(":", 1)
+                key = key.strip().lower().replace(" ", "_")  # Pulisce la chiave per il dizionario
+                value = value.strip()
+                current_rule[key] = value
+            elif "----" in line:  #fine della regola
+                if current_rule:
+                  rules.append(current_rule)
+                current_rule = {} #resetta il dizionario
+
+        if current_rule: #aggiunge l'ultima regola
+            rules.append(current_rule)
+        return rules
+
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante l'esecuzione del comando netsh: {e}")
+        sys.exit(1)
+    except Exception as ex:
+        print(f"Si è verificato un errore imprevisto: {ex}")
+        sys.exit(1)
+
+
+
+
+    
+
+if __name__ == "__main__":
+    # Verifica i privilegi di amministratore prima di procedere.
+    run_as_admin()
+
+    if len(sys.argv) > 1:
+
+        folder_path = sys.argv[1]
+        block_exe_traffic(folder_path)
+        input("Done. Press to esc..")
+    else:
+        print(sys.argv)
+        firewall_rules = list_firewall_rules()
